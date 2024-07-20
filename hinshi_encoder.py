50b6a73d473c4d0c5f52dcd4504e6c9da6cb657b
windows_on_wsl <otomijuf.004@gmail.com>
2024-07-20 20:42:31 +0900

    debug

--- hinshi_encoder.py
+++ hinshi_encoder.py
@@ -61,6 +61,7 @@ def build_hinshi_tokenize(tokenizer, rate=0.5):
             node = node.next
     
     def encode(text, add_special_tokens=True):
+        print('text: ', text)
         tokenized = tokenizer.tokenize(text)
         encoded = tokenizer.encode(text)
         ids = []
